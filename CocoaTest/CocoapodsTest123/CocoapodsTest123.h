0cdfaf04964aeb319c3587c3876ca21e3196e9d9
peitao <peitao@bytedance.com>
2019-01-28 17:49:06 +0800

    add another pod

---
+++ CocoaTest/CocoapodsTest123/CocoapodsTest123.h
@@ -0,0 +1,19 @@
+//
+//  CocoapodsTest123.h
+//  CocoaTest
+//
+//  Created by sjtupt on 2019/1/28.
+//  Copyright © 2019 sjtupt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CocoapodsTest123 : NSObject
+
+- (void)testCocoapodsTest;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CocoaTest/CocoapodsTest123/CocoapodsTest123.m
@@ -0,0 +1,16 @@
+//
+//  CocoapodsTest123.m
+//  CocoaTest
+//
+//  Created by sjtupt on 2019/1/28.
+//  Copyright © 2019 sjtupt. All rights reserved.
+//
+
+#import "CocoapodsTest123.h"
+
+@implementation CocoapodsTest123
+
+- (void)testCocoapodsTest {
+    NSLog(@"CocoapodsTest123");
+}
+@end
